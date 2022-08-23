df021060f6d560c7d480de190f46833e312df599
dezji <86417645+dezji@users.noreply.github.com>
2022-08-22 21:19:01 -0400

    Add files via upload

---
+++ Population.py
@@ -0,0 +1,18 @@
+# Author Name: Deziallia Morris
+# Date: 9/19/2021
+# Program Name: Morris_Population.py
+# Purpose:
+
+# Set up variables and get user input to store in variables
+startingOrganisms = int(input("Starting organisms: "))
+organisms = startingOrganisms
+averageDailyIncrease = float(input("Average daily increase (percent): ")) / 100
+numberOfDays = int(input("Number of days to multiply: "))
+
+# Print columns
+print("Days Approximate\tPopulation")
+
+# Loop for each day, and tab for columns to line up. Format organisms to float
+for day in range(1, numberOfDays + 1):
+    print(day, '\t\t\t\t\t', float(organisms))
+    organisms = (organisms * averageDailyIncrease) + organisms
